65dc942e2fae11c066db3e2e177cb04cb8fb80d7
Vadim Raskhozhev <iamdexpl@gmail.com>
2020-09-03 07:15:19 +0300

    bin/xx2en.bat: added  clause (useful in domain envs.)

--- bin/xx2en.bat
+++ bin/xx2en.bat
@@ -1,4 +1,4 @@
-::@echo off
+@echo off
 
 :: This script renames built-in admin and guest accounts into admin and guest
 :: respectively from whatever they are. Idea is that the names are system
@@ -9,17 +9,17 @@
 wmic exit || exit /b 1
 
 :: users
-wmic useraccount where 'sid like "S-1-5-21-%%-500"' call rename admin
-wmic useraccount where 'sid like "S-1-5-21-%%-501"' call rename guest
+wmic useraccount where "localaccount='true' and sid like 'S-1-5-21-%%-500'" call rename admin
+wmic useraccount where "localaccount='true' and sid like 'S-1-5-21-%%-501'" call rename guest
 
 :: groups
-wmic group where sid="S-1-5-32-544" call rename admins
-wmic group where sid="S-1-5-32-578" call rename hvadmins
-wmic group where sid="S-1-5-32-546" call rename guests
-wmic group where sid="S-1-5-32-569" call rename cryptoops
-wmic group where sid="S-1-5-32-551" call rename backupops
-wmic group where sid="S-1-5-32-547" call rename powerusers 
-wmic group where sid="S-1-5-32-545" call rename users
-wmic group where sid="s-1-5-32-562" call rename dcom
-wmic group where sid="S-1-5-32-555" call rename rdp
-wmic group where sid="S-1-5-32-580" call rename winrm
+wmic group where "localaccount='true' and sid='S-1-5-32-544'" call rename admins
+wmic group where "localaccount='true' and sid='S-1-5-32-578'" call rename hvadmins
+wmic group where "localaccount='true' and sid='S-1-5-32-546'" call rename guests
+wmic group where "localaccount='true' and sid='S-1-5-32-569'" call rename cryptoops
+wmic group where "localaccount='true' and sid='S-1-5-32-551'" call rename backupops
+wmic group where "localaccount='true' and sid='S-1-5-32-547'" call rename powerusers
+wmic group where "localaccount='true' and sid='S-1-5-32-545'" call rename users
+wmic group where "localaccount='true' and sid='s-1-5-32-562'" call rename dcom
+wmic group where "localaccount='true' and sid='S-1-5-32-555'" call rename rdp
+wmic group where "localaccount='true' and sid='S-1-5-32-580'" call rename winrm
