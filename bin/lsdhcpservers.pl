351634e2a77e05bfaa730d7c177448479197a86d
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-07-27 23:07:48 +0300

    bin/lsdhcpservers.pl: corner cases fixed

--- bin/lsdhcpservers.pl
+++ bin/lsdhcpservers.pl
@@ -6,17 +6,19 @@ use feature qw(say);
 
 $, = shift // ': ';
 my ( $ifname, %dhcp );
+
 # TODO use `nmcli -g all device show` since this won't work on CentOS 7
 foreach (`nmcli -g UUID connection show --active`) {
     foreach (`nmcli -g GENERAL.DEVICES,DHCP4 connection show uuid $_`) {
         chomp;
         if (s,^DHCP4:,,) {
             $dhcp{$ifname} = (
-                split / = /, ( grep { /dhcp_server_identifier/ } split / \| / )[0]
+                split / = /,
+                ( grep { /dhcp_server_identifier/ } split / \| / )[0] // ''
             )[-1];
         } else {
             $ifname = $_;
         }
     }
 }
-say $_, $dhcp{$_} foreach sort keys %dhcp;
+say $_, $dhcp{$_} foreach grep { $dhcp{$_} } sort keys %dhcp;
