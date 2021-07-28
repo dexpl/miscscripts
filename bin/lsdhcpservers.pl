03198230d09e9b54922a5bd4d38538a6f1904590
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-07-28 18:05:35 +0300

    bin/lsdhcpservers.pl: reworked using just one mncli(1) call

--- bin/lsdhcpservers.pl
+++ bin/lsdhcpservers.pl
@@ -5,30 +5,20 @@ use warnings;
 use feature qw(say);
 
 $, = shift // ': ';
-my ( $ifname, $item, $flag, @data, %dhcp );
 
-# TODO use `nmcli -g all device show` since this won't work on CentOS 7
-foreach (`nmcli -g all device show`) {
+my $nmcli_cmd = q(nmcli -f GENERAL.DEVICE,DHCP4.OPTION --terse device show);
+my $dhcp_option = q(dhcp_server_identifier);
+my $ifname;
+
+foreach (`$nmcli_cmd`) {
     chomp;
-	$item = {} unless $item;
-	if (my @chunk = split ':') {
-		$item->{$chunk[0]} = join ':', @chunk[1..$#chunk];
-		$flag++ if $chunk[0] eq 'DHCP4';
-	} else {
-		push @data, $item if $flag;
-		($item, $flag) = undef;
-	}
-}
-use Data::Dumper;
-print Dumper @data;
-say $_, $dhcp{$_} foreach sort keys %dhcp;
-__END__
-    if (s,^DHCP4:,,) {
-        $dhcp{$ifname} =
-          ( split / = /, ( grep { /dhcp_server_identifier/ } split / \| / )[0] )
-          [-1];
-    } else {
-        $ifname = $_;
+    if ( my @chunk = split /:/ ) {
+        if ( $chunk[0] eq 'GENERAL.DEVICE' ) {
+            $ifname = $chunk[-1];
+        } else {
+            my ( $name, $value ) = split / = /, join ':',
+              @chunk[ 1 .. $#chunk ];
+            say $ifname, $value if $name eq $dhcp_option;
+        }
     }
 }
-say $_, $dhcp{$_} foreach sort keys %dhcp;
