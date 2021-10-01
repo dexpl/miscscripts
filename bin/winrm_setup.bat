639727574026da2471a36892c381a0f635fcbb37
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-10-01 15:01:40 +0300

    bin/winrm_setup.bat: Allow basic auth only on non-AD machines

--- bin/winrm_setup.bat
+++ bin/winrm_setup.bat
@@ -2,5 +2,7 @@
 
 powershell -NoLogo -Command "& {Set-NetConnectionProfile -NetworkCategory Private}"
 call winrm quickconfig -q -force
+wmic computersystem where name="%computername%" get partofdomain /format:value | findstr -c:"^partofdomain=true$" -ir && exit /b
 call winrm set winrm/config/service @{AllowUnencrypted="true"}
+:: Allow basic auth only on non-AD machines
 call winrm set winrm/config/service/auth @{Basic="true"}
