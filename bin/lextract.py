37500803894d2c5faff53e3634a92329a4b43548
Vadim Raskhozhev <iamdexpl@gmail.com>
2021-12-05 17:34:36 +0300

    bin/lextract.py: 1) use urlparse.urljoin() to convert urls; 2) various cosmetic improvements

--- bin/lextract.py
+++ bin/lextract.py
@@ -5,18 +5,17 @@ import re
 
 try:
     from html.parser import HTMLParser
+    import urllib.parse as up
+    import urllib.request as ureq
 except ImportError:
     from HTMLParser import HTMLParser
-
-try:
-    import urllib.request as rq
-except ImportError:
-    import urllib as rq
+    import urlparse as up
+    import urllib as ureq
 
 class LinkScrape(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
-        [print(re.sub(r_what, r_with, link)) for link in [v if v.find('http') == 0 else baseurl + v for k, v in attrs if k == 'href' if tag == 'a'] if re.search(regex, link)]
+        [print(re.sub(r_what, r_with, link)) for link in [up.urljoin(baseurl, v) for k, v in attrs if k == 'href' if tag == 'a'] if re.search(regex, link)]
 
 if __name__ == '__main__':
     try:
@@ -27,8 +26,8 @@ if __name__ == '__main__':
             r_with = sys.argv[4]
         except IndexError:
             r_what = r_with = ''
-        baseurl = '/'.join(url.split('/')[0:3])
-        LinkScrape().feed(rq.urlopen(url).read().decode('utf-8'))
+        baseurl = up.urljoin(url, '..')
+        LinkScrape().feed(ureq.urlopen(url).read().decode('utf-8'))
     except IndexError:
         # TODO read stdin
-        print('Usage: %s URL [regex]' % sys.argv[0], file=sys.stderr)
+        print('Usage: %s URL [regex] [replace_what] [replace_with]' % sys.argv[0], file=sys.stderr)
