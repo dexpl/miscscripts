a81f25dea4f801e9104677e59ef5152279ff116d
Vadim Raskhozhev <iamdexpl@gmail.com>
2022-05-13 22:27:39 +0300

    bin/cbr.sh: date(1)-compatible date specification's made possible (via date(1) in fact)

--- bin/cbr.sh
+++ bin/cbr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-[ -n "${1}" ] && date_req="?date_req=${1}"
+[ -n "${1}" ] && date_req="?date_req=$(date --date "${*}" +%d/%m/%Y)"
 xmlstarlet sel --net -t -v //ValCurs/@Date -n -m '//CharCode[text() = "USD" or text() = "EUR" or text() = "CNY" or text() = "INR"]' -v 'concat(../Nominal, " ", ../Name, ": ", translate(../Value, ",", "."))' -n http://www.cbr.ru/scripts/XML_daily.asp${date_req} | dos2unix | column -ts ':'
